f210cd9f0097626a5c91d7eaa78f3d72ef466dd0
Ramon Chancay <ramon.chancay@gmail.com>
2018-11-04 17:59:04 -0500

    switcher

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,15 +12,25 @@ import Footer from '../components/Footer/index'
 
 const IndexPage = ({ data, pathContext }) => (
   <Layout locale={pathContext.locale}>
-    <ul className="list-reset w-full flex justify-center p-4">
+
+    <ul className="list-reset w-full flex justify-center bg-blue">
+    {pathContext.locale === 'es' && (
       <li className="p-1">
-        <Link className="no-underline hover:no-underline font-black" to="/">EN</Link>
+        <Link className="text-2xl no-underline hover:no-underline font-black" to="/">
+        <span aria-label="English version">🇺🇸</span> <span className="text-white">English Version</span>
+        </Link>
       </li>
-      <li className="flex justify-center items-center conten-center"> - </li>
+    )}
+      {pathContext.locale === 'en' && (
       <li className="p-1">
-        <Link className="no-underline hover:no-underline font-black" to="/es">ES</Link>
+        <Link className="text-2xl no-underline hover:no-underline font-black" to="/es">
+          <span aria-label="Spanish version">🇪🇸</span> <span className="text-white">Version en español</span>
+        </Link>
       </li>
+    )}
+    
     </ul>
+
     <Header locale={pathContext.locale} />
     <About locale={pathContext.locale} />
     <Posts locale={pathContext.locale} posts={data.allMediumPost.edges} />
