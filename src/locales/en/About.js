40e0ca10bf28620dc6ff2edcc2e19974e8104178
Ramon Chancay <ramon.chancay@gmail.com>
2019-01-20 01:33:00 -0500

    add last jobs for experience section

--- src/locales/en/About.js
+++ src/locales/en/About.js
@@ -10,4 +10,35 @@ export default {
     `I enjoy building things with code in my free time and helping developers improve their skills, I write in medium in Spanish.`,
     `I'm a fan of remote work and Javascript technologies, you can ask for help with Javascript React, Vue and Nodejs.`,
   ],
+  experience: [
+    {
+      title: 'Frontend Engineer',
+      company: 'EveryMundo',
+      location: 'Ecuador',
+      from: 'Sep 2018',
+      description: [
+        'Develop new user-facing features using React.js.',
+        'Build reusable components.',
+        'Translate designs and wireframes into high quality code.',
+        'Optimize components for maximum performance across a vast array of web-capable devices and browsers.',
+        'Create unit testing for each delivered module.',
+        'Maintain and improve the existing code base',
+      ],
+    },
+    {
+      title: 'Full Stack Engineer',
+      company: 'Diario El Universo',
+      location: 'Ecuador',
+      from: 'Jan 2015',
+      to: 'Nov 2018',
+      time: '3 yrs 11 mos',
+      description: [
+        'Design and maintain new features of the website.',
+        'Make architecture decisions for new developments.',
+        'Document and implement new technologies.',
+        'Improve the performance of the website.',
+        'Maintenance of internal subscription systems to the printed newspaper using Nodejs.',
+      ],
+    },
+  ],
 }
