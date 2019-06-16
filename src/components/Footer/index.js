bc5f96e489d16a99f3ead0d5b4cdb54fec55e16d
Ramon Chancay <ramon.chancay@gmail.com>
2019-06-16 18:42:31 -0500

    adding buttons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="text-2xl font-bold w-full md:w-1/3 py-2 flex justify-center md:justify-start">
           <h3 className="font-sans">Ramón Chancay Ortega</h3>
         </div>
-        <div className="w-full md:w-1/3 py-2 flex justify-center items-center content-center">
+        <div className="order-last md:order-none w-full md:w-1/3 py-2 flex justify-center items-center content-center">
           <small className="text-md font-bold font-sans">
             Made with <span className="text-red-500">❤</span> using GatsbyJS and
             Netlify
