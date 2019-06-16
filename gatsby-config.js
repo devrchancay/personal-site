dd7d8c6ea17438b55ff66e4d63925e92404f6c47
Ramón Chancay Ortega <ramon.chancay@gmail.com>
2019-06-16 00:27:23 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
                   node {
                     ... on Repository {
                       name
-                      description: descriptionHTML
+                      descriptionHTML
                       url
                       stargazers{
                         totalCount
