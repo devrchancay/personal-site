888344de1da1f153cd4a194ec4a210c006dce15a
Ramon Chancay <ramon.chancay@gmail.com>
2019-11-15 21:11:33 -0500

    blog

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -77,7 +77,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "DD MMMM YYYY", locale: "es")
         image {
           id
           childImageSharp {
